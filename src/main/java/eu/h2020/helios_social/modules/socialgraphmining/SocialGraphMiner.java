bbc303e010a89278991090e54b532c3d445698d9
mkrasanakis <maniospas@hotmail.com>
2021-03-04 14:10:39 +0200

    fixed remodulariation error of previous comit

--- src/main/java/eu/h2020/helios_social/modules/socialgraphmining/SocialGraphMiner.java
+++ src/main/java/eu/h2020/helios_social/modules/socialgraphmining/SocialGraphMiner.java
@@ -15,6 +15,26 @@ import eu.h2020.helios_social.core.contextualegonetwork.Node;
  */
 public abstract class SocialGraphMiner {
 	public enum InteractionType {SEND, RECEIVE, RECEIVE_REPLY};
+	
+	public static class SocialGraphMinerParameters {
+		private HashMap<String, Object> params;
+		public SocialGraphMinerParameters() {
+			params = new HashMap<String, Object>();
+		}
+		public Object get(String key) {
+			return params.get(key);
+		}
+		public SocialGraphMinerParameters getNested(String key) {
+			return (SocialGraphMinerParameters)get(key);
+		}
+		public void put(String key, Object value) {
+			params.put(key, value);
+		}
+		public Set<String> getKeys() {
+			return params.keySet();
+		}
+	}
+	
 	private ContextualEgoNetwork contextualEgoNetwork;
 	protected SocialGraphMiner(ContextualEgoNetwork contextualEgoNetwork) {
 		if(contextualEgoNetwork==null)
